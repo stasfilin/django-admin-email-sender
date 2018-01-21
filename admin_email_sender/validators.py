f7c0aa91126e5bdc32815bec76ac0d706d452004
Stanislav Filin <stasfilinmusic@gmail.com>
2018-01-21 23:30:29 +0200

    Fix validator exception
    Signed-off-by: Stanislav Filin <stasfilinmusic@gmail.com>

--- admin_email_sender/validators.py
+++ admin_email_sender/validators.py
@@ -5,6 +5,6 @@ from django.utils.translation import gettext_lazy as _
 def validate_tags(value):
     if '@body' not in value:
         raise ValidationError(
-            _('I can find @body tag'),
+            _('I cant find @body tag'),
             params={'value': value},
         )
